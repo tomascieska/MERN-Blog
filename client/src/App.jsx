e60b533bf6a1759432d428e2732e53697ab25ca1
tomascieska <tomas.cieska@gmail.com>
2024-01-21 20:48:14 +0000

    install react js, tailwind css and first template

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-red-600">
+        Click on the Vite and React logos to learn more
+      </h1>
+      
+    </>
+  )
+}
+
+export default App
